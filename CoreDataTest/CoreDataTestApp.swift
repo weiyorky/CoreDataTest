cd7ae2a3b6b67ccc9e9166e4840d4c1b2c31cbd7
York <weiyorky@gmail.com>
2024-11-28 17:47:16 -0500

    Initial Commit

---
+++ CoreDataTest/CoreDataTestApp.swift
@@ -0,0 +1,20 @@
+//
+//  CoreDataTestApp.swift
+//  CoreDataTest
+//
+//  Created by york on 11/28/24.
+//
+
+import SwiftUI
+
+@main
+struct CoreDataTestApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
---
+++ CoreDataTestTests/CoreDataTestTests.swift
@@ -0,0 +1,17 @@
+//
+//  CoreDataTestTests.swift
+//  CoreDataTestTests
+//
+//  Created by york on 11/28/24.
+//
+
+import Testing
+@testable import CoreDataTest
+
+struct CoreDataTestTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
